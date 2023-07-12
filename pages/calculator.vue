4ed767fa4e30458c6babceaa66cf1b8504a16227
Swakresna Edityomurti <swakresna.edityomurti@amaan.co.id>
2023-07-12 20:10:04 +0700

    Initial commit

---
+++ pages/calculator.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="main-container">
+        <h1>Hitung Kebutuhan Kalorimu.</h1>
+        <form>
+            <div>
+                <label>
+                    <div>
+                        <input type="number" placeholder="No. Handphone" v-model="phoneNumber" />
+                    </div>
+                </label>
+                <label>
+                    <div>
+                        <input type="text" placeholder="city" v-model="phoneNumber" />
+                    </div>
+                </label>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script setup lang="ts">
+useHead({
+    title: 'Higlo | Hitung kalorimu'
+})
+
+const phoneNumber = ref('')
+const city = ref('')
+
+const weight = ref('')
+const height = ref('')
+const activity = ref('')
+
+</script>
